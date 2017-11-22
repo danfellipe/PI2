d06ec62097e9b2b97afa65b289b268da71e0f69c
Jean Silva <je-francisco@hotmail.com>
2017-11-22 21:58:11 -0200

    Busca de produtos e etc

---
+++ pesquisar.php
@@ -0,0 +1,9 @@
+<?php
+	
+include('../db/bancodedados.php');
+	
+	$pesquisar = $_POST['pesquisar'];
+	$resultBusca = odbc_exec($db, "SELECT * FROM produto WHERE nomeProduto LIKE '%$pesquisar%'");
+	$resultado = odbc_fetch_array($resultBusca);
+		echo "Nome do Produto " . $resultado['nomeProduto'] . "<br>";
+?>
